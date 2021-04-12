cab77a8cc8b33c721c1c3dbdf9c608c1eee80670
相馬由衣 <somayui@yuiMacBook-Pro.local>
2021-04-12 20:01:35 +0900

    mosha

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { Fragment, useState } from 'react';
+import Title from './Title';
+import Form from './Form';
+import List from './List';
+import { nanoid } from 'nanoid';
+
+const App = () => {
+    const [name, setName] = useState("yui");
+
+    const [todos, setTodos] = useState([
+        {
+            content: "掃除する", 
+            id:nanoid(),
+        },
+        {
+            content: "洗濯する", 
+            id:nanoid()
+        },
+        {
+            content:"料理する", 
+            id:nanoid()
+        },
+    ]);
+
+    const addTodo = (text) => {
+        setTodos(...todos, {content:text, id:nanoid()});
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter((todo) => {todo.id !== id}))
+    };
+
+};
+
+export default App;
